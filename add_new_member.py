eb4f1dee847d7c812021651d3129dbc777aa479a
Mike <mdrob@mail.ru>
2017-11-19 15:51:24 +0300

    рефакторинг нового абонента через parametr + change signature

--- add_new_member.py
+++ add_new_member.py
@@ -18,80 +18,92 @@ class add_new_member(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_new_member(wd)
+        self.create_new_member(wd, firstname="Mike", middlename="Sedrick", lastname="Dashman", nickname="Miki", title="Finger", company="MIT", address="CA, Los Altos, 24", home="none",
+                               mobile="1234567890", workphone="(213)1235467", fax="none", email="alo1@ya.com", email2="mk@mit.com", email3="none",
+                               homepage="www.sedrick.com", birthday="//div[@id='content']/form/select[1]//option[20]",
+                               birthmonth="//div[@id='content']/form/select[2]//option[6]", birthyear="1986",
+                               annyday="//div[@id='content']/form/select[3]//option[20]",
+                               annimonth="//div[@id='content']/form/select[4]//option[6]", anniyear="2026", address2="CA, Los Altos, Hrandi, 25",
+                               homephone2="Campos", notes="none")
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_new_member(self, wd):
+    def create_new_member(self, wd, firstname, middlename, lastname, nickname, title, company, address, home, mobile,
+                          workphone, fax, email, email2, email3, homepage, birthday, birthmonth, birthyear, annyday,
+                          annimonth, anniyear, address2, homephone2, notes):
         # add_new_member
         wd.find_element_by_link_text("add new").click()
         # enter_member_form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Mike")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Sedrick")
+        wd.find_element_by_name("middlename").send_keys(middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Dashman")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Miki")
+        wd.find_element_by_name("nickname").send_keys(nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Finger")
+        wd.find_element_by_name("title").send_keys(title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("MIT")
+        wd.find_element_by_name("company").send_keys(company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("CA, Los Altos, 24")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("none")
+        wd.find_element_by_name("home").send_keys(home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("1234567890")
+        wd.find_element_by_name("mobile").send_keys(mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("(213)1235467")
+        wd.find_element_by_name("work").send_keys(workphone)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("none")
+        wd.find_element_by_name("fax").send_keys(fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("alo1@ya.com")
+        wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("mk@mit.com")
+        wd.find_element_by_name("email2").send_keys(email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("none")
+        wd.find_element_by_name("email3").send_keys(email3)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("www.sedrick.com")
+        wd.find_element_by_name("homepage").send_keys(homepage)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[20]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[20]").click()
+            wd.find_element_by_xpath(birthday).click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[6]").click()
+            wd.find_element_by_xpath(birthmonth).click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1986")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[2]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[2]").click()
+        wd.find_element_by_name("byear").send_keys(birthyear)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[20]").is_selected():
+            wd.find_element_by_xpath(annyday).click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").is_selected():
+            wd.find_element_by_xpath(annimonth).click()
         wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(anniyear)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("CA, Los Altos, Hrandi, 25")
+        wd.find_element_by_name("address2").send_keys(address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("Campos")
+        wd.find_element_by_name("phone2").send_keys(homephone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys()
+        wd.find_element_by_name("notes").send_keys(notes)
         # send_member_form
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
