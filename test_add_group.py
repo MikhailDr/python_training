7b1e8be7aeab0ea8951bb81db65d817ec6568dd4
Mike <mdrob@mail.ru>
2017-11-21 20:15:33 +0300

    Переход в pytest import+добавили фикстуру Application+addfinalizer

--- test_add_group.py
+++ test_add_group.py
@@ -1,32 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+import pytest
 from group import Group
-from application import  Application
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
     
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group (name="People", header="example1", footer="Hi all this book"))
-        self.app.logout()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group (name="People", header="example1", footer="Hi all this book"))
+    app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group (name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group (name="", header="", footer=""))
+    app.logout()
 
 if __name__ == '__main__':
     unittest.main()
