f1175adce6a1253ebbbf4acd1db294b86d94e9f2
Mike <mdrob@mail.ru>
2017-11-23 20:48:49 +0300

    Создали вспомогательный класс Contact, добавили тест на создание контакта

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -4,7 +4,7 @@ from model.contacts import Contacts
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.group.create_new_contact(Contacts(firstname="Mike", middlename="Sedrick", lastname="Dashman", nickname="Miki", title="Finger", company="MIT", address="CA, Los Altos, 24", home="none",
+    app.contact.create_new_contact(Contacts(firstname="Mike", middlename="Sedrick", lastname="Dashman", nickname="Miki", title="Finger", company="MIT", address="CA, Los Altos, 24", home="none",
                                mobile="1234567890", workphone="(213)1235467", fax="none", email="alo1@ya.com", email2="mk@mit.com", email3="none",
                                homepage="www.sedrick.com", birthday="//div[@id='content']/form/select[1]//option[20]",
                                birthmonth="//div[@id='content']/form/select[2]//option[6]", birthyear="1986",
