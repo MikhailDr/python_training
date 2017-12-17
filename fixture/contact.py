007315e0d8ef71047a0be82e20d2d98e8784b44e
Mike <mdrob@mail.ru>
2017-12-17 14:12:47 +0300

    'NoneType' object has no attribute 'group'

--- fixture/contact.py
+++ fixture/contact.py
@@ -91,8 +91,9 @@ class ContactHelper:
                 firstname = cells[2].text
                 lastname = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, home=all_phones[0], mobile=all_phones[1], workphone=all_phones[2], homephone2=all_phones[3]))
+                all_phones = cells[5].text
+                all_email = cells[4].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones, all_email_from_home_page=all_email))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -119,17 +120,24 @@ class ContactHelper:
         mobile = wd.find_element_by_name("mobile").get_attribute("value")
         workphone = wd.find_element_by_name("work").get_attribute("value")
         homephone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id, home=home,
-                       mobile=mobile, workphone=workphone, homephone2=homephone2)
+                       mobile=mobile, workphone=workphone, homephone2=homephone2, email=email, email2=email2, email3=email3)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
-        self.open_contact_to_edit_by_index(index)
+        self.open_contact_view_by_index(index)
         text = wd.find_element_by_id("content").text
+        text2 = wd.find_element_by_xpath("//div[@id='content']/a").text
         home = re.search("H: (.*)", text).group(1)
         mobile = re.search("M: (.*)", text).group(1)
         workphone = re.search("W: (.*)", text).group(1)
         homephone2 = re.search("P: (.*)", text).group(1)
-        return Contact(home=home,mobile=mobile, workphone=workphone, homephone2=homephone2)
+        email = re.search("(.*)", text2).group(1)
+        email2 = re.search("(.*)", text2).group(1)
+        email3 = re.search("(.*)", text2).group(1)
+        return Contact(home=home,mobile=mobile, workphone=workphone, homephone2=homephone2, email=email, email2=email2, email3=email3)
 
 
--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,7 @@ class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                             address=None, home=None, mobile=None, workphone=None, fax=None, email=None, email2=None,
                             email3=None, homepage=None, birthday=None, birthmonth=None, birthyear=None, anniday=None,
-                          annimonth=None, anniyear=None, address2=None, homephone2=None, notes=None, id=None):
+                          annimonth=None, anniyear=None, address2=None, homephone2=None, notes=None, id=None, all_phones_from_home_page=None, all_email_from_home_page=None):
 
         self.firstname = firstname
         self.middlename = middlename
@@ -32,6 +32,8 @@ class Contact:
         self.homephone2 = homephone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_email_from_home_page = all_email_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
