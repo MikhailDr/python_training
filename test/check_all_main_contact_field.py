64b79f2f7508f961f6dd5c51ee96f896ac87e4a8
Mike <mdrob@mail.ru>
2017-12-18 22:38:55 +0300

    'NoneType' object has no attribute 'group'

--- test/check_all_main_contact_field.py
+++ test/check_all_main_contact_field.py
@@ -1,5 +1,5 @@
 import re
-
+#
 def test_contact_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
