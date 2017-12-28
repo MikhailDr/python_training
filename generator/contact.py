0ef68b198b7ccba797d21953ab79c47b17b460ea
Mike <mdrob@mail.ru>
2017-12-28 18:29:18 +0300

    Реализовал генераторы тестовых данных для групп и контактов, сохраняющие созданные данные в файл в формате JSON. Используя библиотеку jsonpickle для сохранения структуры объектов в формат JSON

--- generator/contact.py
+++ generator/contact.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from model.group import Group
+from model.contact import Contact
 import random
 import string
 import os.path
@@ -8,13 +8,13 @@ import getopt
 import sys
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
 except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
-n = 5
-f = "data/groups.json"
+n = 2
+f = "data/contacts.json"
 
 for o, a in opts:
     if o == "-n":
@@ -24,12 +24,12 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits + " "*5
     return prefix + " ".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+testdata = [
+    Contact(firstname=random_string("firstname", 7), lastname=random_string("lastname", 10))
     for i in range(n)
 ]
 
